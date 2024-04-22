d42cbe36a5aa72d74199d68c1406634be1381f13
Mike Davis <mike.davis@psyvad.com>
2024-04-22 15:50:22 -0600

    changed port

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ app.get("/", (req, res) => {
   res.send("Amber Davis");
 });
 
-const port = 3000;
+const port = 3500;
 
 app.listen(process.env.port || port);
 console.log("Web Server is listening at port " + (process.env.port || port));
