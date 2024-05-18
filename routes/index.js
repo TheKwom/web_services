5da52e1e75ea12eb25e3baea32f89667b077c494
Mike Davis <mike.davis@psyvad.com>
2024-05-18 11:10:21 -0600

    fixed swagger bug

--- routes/index.js
+++ routes/index.js
@@ -1,19 +1,7 @@
 const express = require("express");
 const router = express.Router();
-const swaggerUi = require("swagger-ui-express");
-const swaggerDocument = require("../swagger-output.json");
 
+router.use("/", require("./swagger"));
 router.use("/contacts", require("./contacts"));
-router.use("/api-docs", swaggerUi.serve);
-router.get("/api-docs", swaggerUi.setup(swaggerDocument));
-router.use(
-  "/",
-  (docData = (req, res) => {
-    let docData = {
-      documentationURL: "https://cse341-spring24-uwqp.onrender.com/api-docs",
-    };
-    res.send(docData);
-  })
-);
 
 module.exports = router;
